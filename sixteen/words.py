afd117e74674c85a4bed36a8e4f10ce41f370564
tim <tdixon51793@gmail.com>
2012-04-05 03:04:23 -0500

    docstring, from the docs.

--- sixteen/words.py
+++ sixteen/words.py
@@ -6,6 +6,16 @@ from bitstring import Bits
 class Word(object):
     """These objects represent DCPU-16 words, and they can be interpreted
     either as opcodes or as literals.
+
+    DCPU-16 words are sixteen-bit unsigned.
+
+    From the docs:
+    > Instructions are 1-3 words long and are fully defined by the first word.
+    > In a basic instruction, the lower four bits of the first word of the
+    > instruction are the opcode, and the remaining twelve bits are split into
+    > two six bit values, called a and b. a is always handled by the processor
+    > before b, and is the lower six bits. In bits (with the least significant
+    > being last), a basic instruction has the format: bbbbbbaaaaaaoooo
     """
     def __init__(self, word):
         "Given a Bits or a BitArray, initialize this word."
