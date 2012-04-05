6ad5dd99a946bd1dd8e63ccd24bdc0de70830e6d
tim <tdixon51793@gmail.com>
2012-04-05 00:50:54 -0500

    basic setup.py, .gitignore, and empty __init__.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from glob import glob
+
+
+setup(
+    name = "sixteen",
+    version = "0.00.0dev",
+    author = "startling",
+    author_email = "tdixon51793@gmail.com",
+    packages = ["sixteen"],
+    scripts = glob("scripts/*"),
+)
