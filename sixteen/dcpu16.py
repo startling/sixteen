52ba936d46b118e65179f68ffe75be4d7ac4a460
tim <tdixon51793@gmail.com>
2012-04-05 18:52:47 -0500

    implemented XOR

--- sixteen/dcpu16.py
+++ sixteen/dcpu16.py
@@ -206,3 +206,8 @@ class DCPU16(object):
         a_set, a_get = self.values[a]
         _, b_get = self.values[b] 
         a_set(a_get() | b_get())
+
+    def XOR(self, a, b):
+        a_set, a_get = self.values[a]
+        _, b_get = self.values[b] 
+        a_set(a_get() ^ b_get())
