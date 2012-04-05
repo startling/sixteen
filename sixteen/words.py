edf04cd6014fed4198da49c949752b5d76cc73a8
tim <tdixon51793@gmail.com>
2012-04-05 13:27:10 -0500

    revamped sixteen.words
    we don't need a class! wooh

--- sixteen/words.py
+++ sixteen/words.py
@@ -1,46 +1,34 @@
 # -*- coding: utf-8 -*-
+"""These functions help represent DCPU-16 words, and they can be interpreted
+either as opcodes or as literals.
 
-from bitstring import BitArray
+DCPU-16 words are sixteen-bit unsigned.
+
+From the docs:
+> Instructions are 1-3 words long and are fully defined by the first word.  In
+> a basic instruction, the lower four bits of the first word of the instruction
+> are the opcode, and the remaining twelve bits are split into two six bit
+> values, called a and b. a is always handled by the processor before b, and is
+> the lower six bits. In bits (with the least significant being last), a basic
+> instruction has the format: bbbbbbaaaaaaoooo
+"""
+
+from bitstring import Bits
 
 
-class Word(object):
-    """These objects represent DCPU-16 words, and they can be interpreted
-    either as opcodes or as literals.
+def from_hex(word):
+    return int(word, base=16)
 
-    DCPU-16 words are sixteen-bit unsigned.
 
-    From the docs:
-    > Instructions are 1-3 words long and are fully defined by the first word.
-    > In a basic instruction, the lower four bits of the first word of the
-    > instruction are the opcode, and the remaining twelve bits are split into
-    > two six bit values, called a and b. a is always handled by the processor
-    > before b, and is the lower six bits. In bits (with the least significant
-    > being last), a basic instruction has the format: bbbbbbaaaaaaoooo
+def as_opcode(word):
+    """Interpret this word as an opcode. from the docs:
+
+    'a basic instruction has the format: bbbbbbaaaaaaoooo'
+
+    This method returns three integers: o, a, and b.
     """
-    def __init__(self, word):
-        "Given a Bits or a BitArray, initialize this word."
-        self._word = word
-
-    @classmethod
-    def from_hex(cls, word):
-        "Initialize a Word given a string of hexadecimal."
-        return cls(BitArray("0x" + word))
-
-    def as_opcode(self):
-        """Interpret this word as an opcode. from the docs:
-
-        'a basic instruction has the format: bbbbbbaaaaaaoooo'
-
-        This method returns three integers: o, a, and b.
-        """
-        b = self._word[:6]
-        a = self._word[6:-4]
-        o = self._word[-4:]
-        return o.uint, a.uint, b.uint
-    
-    def as_literal(self):
-        "Interpret this word as a literal; returns one integer."
-        return self._word.uint
-
-    def __str__(self):
-        return self._word.hex
+    bitarray = Bits("0x%04x" % word)
+    b = bitarray[:6]
+    a = bitarray[6:-4]
+    o = bitarray[-4:]
+    return o.uint, a.uint, b.uint
