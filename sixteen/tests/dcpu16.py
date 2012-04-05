4f879b07452beb9df66a8b91208a3627c28e7cb3
tim <tdixon51793@gmail.com>
2012-04-05 14:30:54 -0500

    test register pointers

--- sixteen/tests/dcpu16.py
+++ sixteen/tests/dcpu16.py
@@ -37,3 +37,16 @@ class TestDCPU16(unittest.TestCase):
         # and then check things
         self.assertEquals(self.cpu.registers["X"], 0x0004)
         self.assertEquals(self.cpu.registers["A"], 0x001a)
+
+    def test_set_register_pointer(self):
+        # set A, 0x0020 
+        self.cpu[0] = 0x7c01
+        self.cpu[1] = 0x0020
+        # set the address A points to to be 0xbeef
+        self.cpu[2] = 0x7c81
+        self.cpu[3] = 0xbeef
+        # run
+        self.cpu.cycle()
+        self.cpu.cycle()
+        # make sure we made the address 0x0020 be 0xbeef
+        self.assertEquals(self.cpu[0x0020], 0xbeef)
