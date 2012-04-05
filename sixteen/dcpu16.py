8906e769402330d2f48195dd7edb545d6409d3cb
tim <tdixon51793@gmail.com>
2012-04-05 03:53:37 -0500

    DCPU16 objects do things, now.

--- sixteen/dcpu16.py
+++ sixteen/dcpu16.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from sixteen.words import Word
+
 
 class DCPU16(object):
     opcodes = {
@@ -9,10 +11,30 @@ class DCPU16(object):
         0x0b: "XOR", 0x0c: "IFE", 0x0d: "IFN", 0x0e: "IFG", 0x0f: "IFB",
     }
 
-    registers = {
+    _registers = {
         # basic registers
         "A": 0x0000, "B": 0x0000, "C": 0x0000, "X": 0x0000, "Y": 0x0000,
         "Z": 0x0000, "I": 0x0000, "J": 0x0000,
         # special registers
         "PC": 0x000, "SP": 0xffff, "O": 0x0000,
     }
+
+    def __init__(self):
+        # initialize RAM with empty words. None will be treated as empty 0000
+        # words everywhere, because having 0x10000 Words around is expensive
+        # right now.
+        self.RAM = [None] * 0x10000
+        # copy my own `registers` dict.
+        self.registers = self._registers.copy()
+
+    def __getitem__(self, n):
+        "Get the word at a given address."
+        return self.RAM[n] or Word.from_hex("0000")
+
+    def __setitem__(self, n, hex_value):
+        "Set the word at a given address to a hex value."
+        self.RAM[n] = Word.from_hex(hex_value)
+
+    def dump(self):
+        "Return a friendly dump of the RAM."
+        return [w or "0000" for w in self.RAM]
