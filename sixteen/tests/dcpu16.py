0759a58abb56b6f2170ef5bc1ec1876eed3e96f3
tim <tdixon51793@gmail.com>
2012-04-05 05:21:39 -0500

    test for the SET opcode.

---
+++ sixteen/tests/dcpu16.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from sixteen.dcpu16 import DCPU16
+
+
+class TestDCPU16(unittest.TestCase):
+    def setUp(self):
+        self.cpu = DCPU16()
+
+    def test_set_next_word(self):
+        # set A to the next word.
+        self.cpu[0] = "7c01"
+        # set the next word to 0x0030 / 48
+        self.cpu[1] = "0030"
+        # run for a cycle
+        self.cpu.cycle()
+        # and then assert that the register was assigned to.
+        self.assertEquals(self.cpu.registers["A"], 0x0030)
