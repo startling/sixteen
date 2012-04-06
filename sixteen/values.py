907d43a33f490f99bdc8d0e7c653548093b6ae0e
tim <tdixon51793@gmail.com>
2012-04-06 14:10:20 -0500

    wrote a decorator to simplify keeping track of consumed words.
    it's values.consume, btw.

--- sixteen/values.py
+++ sixteen/values.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from functools import wraps
+
 
 class Box(object):
     """A base class that defines the Box interface with some sane defaults.
@@ -11,9 +13,15 @@ class Box(object):
     to do is to save "key"and "container" attributes, which are used to look up
     or set things in "get" and "set".
 
+    There's also a `consumed` attribute that starts at None and should be set
+    to an int if the Box gets a new word. There's also a @consume decorator
+    (scroll down a bit) that sets it and passes it to the "__init__".
+
     This class (it *is* a base class) doesn't define "__init__", so you'll need
     to subclass and define it yourself.
     """
+    consumed = None
+
     def get(self):
         """'get' is called to retrieve the contained value. Nothing
         destructive should happen here. Everything that gets the next word etc
@@ -24,6 +32,14 @@ class Box(object):
     def set(self, value):
         "This is what gets called when something tries to change this value."
         self.container[self.key] = value
+
+
+def consume(fn):
+    @wraps(fn)
+    def initialize(self, cpu):
+        self.consumed = cpu.get_next()
+        fn(self, cpu, self.consumed)
+    return initialize
 
 
 class Register(object):
@@ -56,9 +72,10 @@ class Register(object):
         """Return a box that gets and sets the register the sum of this
         register and the next word points to.
         """
-        def r_init(s, cpu):
+        @consume
+        def r_init(s, cpu, next_word):
             s.container = cpu.RAM
-            s.key = cpu.registers[self.name] + cpu.get_next()
+            s.key = cpu.registers[self.name] + next_word
             # handle overflow
             if s.key >= len(cpu.RAM) - 1:
                 s.key -= (cput.RAM - 1)
@@ -69,8 +86,9 @@ class Register(object):
 
 class NextWord(Box):
     "0x1f: next word (literal)"
-    def __init__(self, cpu):
-        self.value = cpu.get_next()
+    @consume
+    def __init__(self, cpu, next_word):
+        self.value = next_word
     
     def get(self):
         return self.value
@@ -87,10 +105,11 @@ class NextWord(Box):
 
 class NextWordAsPointer(Box):
     "0x1e: [next word]"
-    def __init__(self, cpu):
+    @consume
+    def __init__(self, cpu, next_word):
         "Get and set to and from the address stored in the next word."
         self.container = cpu.RAM
-        self.key = cpu.get_next()
+        self.key = next_word
 
 
 def ShortLiteral(n):
