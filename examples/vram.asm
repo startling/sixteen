03157b503398b1116c4fcbc54959fa6f4f2a32de
tim <tdixon51793@gmail.com>
2012-04-08 15:44:25 -0500

    added a vram assembly example.

---
+++ examples/vram.asm
@@ -0,0 +1,12 @@
+; basic memory-mapped output test
+set [0x8000], 104
+set [0x8001], 101
+set [0x8002], 108
+set [0x8003], 108
+set [0x8004], 111
+set [0x8005], 32
+set [0x8006], 119
+set [0x8007], 111
+set [0x8008], 114
+set [0x8009], 108
+set [0x800a], 100
