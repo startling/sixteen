b79d12b85639b1b839fc61db6fe4bdad529c625c
tim <tdixon51793@gmail.com>
2012-04-09 04:32:56 -0500

    implemented memory-mapped input.

--- sixteen/memorymap.py
+++ sixteen/memorymap.py
@@ -2,18 +2,26 @@
 
 
 class MemoryMap(object):
-    def __init__(self, number, callbacks, initial=0x0000):
-        """Given a number of cells, a list of pairs of ranges to callbacks, and
-        an optional initialization value, make a MemoryMap object. Here's how
-        the callbacks work:
+    def __init__(self, number, write=None, read=None, initial=0x0000):
+        """Given a number of cells, two optional list of pairs of ranges to
+        callbacks, and an optional initialization value, make a MemoryMap
+        object. Here's how the write_callbacks work:
 
-        "callbacks" should be a list like this: [
+        "write_callbacks" should be a list like this: [
             ((0, 10), call)
         ], where "call" is a function that gets called whenever a value stored
         at 0-10 gets changed. The callback gets called with the changed value's
         index and the new value as its arguments.
+
+        Read callbacks work similarly; they get called whenever a item is
+        accessed, and must return a value.
         """
-        self.callbacks = callbacks
+        if read == None:
+            read = []
+        if write == None:
+            write = []
+        self.read_callbacks = read
+        self.write_callbacks = write
         self.number = number
         self._map = [initial] * number
 
@@ -35,17 +43,40 @@ class MemoryMap(object):
                 n = self.number + n
             self._map[n] = value
             # check each callback
-            for (start, end), callback in self.callbacks:
+            for (start, end), callback in self.write_callbacks:
                 # if it's within the bounds, call the callback
                 if n >= start and n < end:
                     callback(n, value)
 
     def __getitem__(self, n):
-        return self._map[n]
+        # if this is a slice object
+        if isinstance(n, slice):
+            # remove the Nones in the slice
+            args = [r for r in (n.start, n.stop, n.step) if r != None]
+            # and then call range with the slice's arguments.
+            return [self[x] for x in range(*args)]
+        else:
+            # make sure that n is in bounds
+            if n >= self.number or n < - self.number:
+                raise IndexError("list index out of range")
+            elif n < 0:
+                n = self.number + n
+            # check each callback
+            for (start, end), callback in self.read_callbacks:
+                # if it's within the bounds, call the callback
+                if n >= start and n < end:
+                    return callback(n)
+            else:
+                # if it didn't get any of the callbacks, do nothing unusual.
+                return self._map[n]
 
     def __len__(self):
         return self.number
 
-    def register(self, (start, end), callback):
-        "Register a new callback."
-        self.callbacks.append((start, end), callback)
+    def register_write(self, (start, end), callback):
+        "Register a new write callback."
+        self.write_callbacks.append((start, end), callback)
+
+    def register_read(self, (start, end), callback):
+        "Register a new read callback."
+        self.read_callbacks.append((start, end), callback)
