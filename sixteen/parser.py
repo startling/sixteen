f0c5407e6f87279e55f19d8601483368433910a0
tim <tdixon51793@gmail.com>
2012-04-09 01:08:08 -0500

    implemented a conceptually simple parsing framework, nicer than the last
    its based around two idea: regular expression matching and Defer.
    
    Any parse function can raise Defer to tell the parser that it doesn't
    know how to parse the thing it was given. This is wrapped up in a nice
    api that lets us use a decorator to assign each parse function a regular
    expression. Then it wraps each parse function in a closure that raises a
    Defer if its regular expression doesn't match. Neat, huh?

--- sixteen/parser.py
+++ sixteen/parser.py
@@ -1,65 +1,69 @@
 # -*- coding: utf-8 -*-
 
 import re
-from sixteen.dcpu16 import DCPU16
-from sixteen.words import from_opcode
-from sixteen.utilities import OpcodeError
-from ast import literal_eval
+from functools import wraps, partial
 
 
 class _meta_parser(type):
-    """A metaclass that collects all the @parse methods into a list of pairs
-    named __parse_methods__; we'll use this for Parsers.
-    
-    It also adds all of the @preprocess-ed methods and adds them to
-    self.__preprocessors__.
-    """
-    def __new__(cls, name, bases, dictionary):
-        __parse_methods__ = []
-        __preprocessors__ = []
-        for k, v in dictionary.iteritems():
-            if getattr(v, "pattern", None):
-                __parse_methods__.append((v.pattern, v))
-            if getattr(v, "is_a_preprocessor", None):
-                __preprocessors__.append(v)
-        dictionary["__parse_methods__"] = __parse_methods__
-        dictionary["__preprocessors__"] = __preprocessors__
-        return type(name, bases, dictionary)
-
-
-def parse(pattern):
-    """Given a pattern to use as a regular expression, return a decorator that
-    sets that function's "pattern" attribute to that pattern.
-    """
-    def parser_decorator(fn):
-        fn.pattern = pattern
-        return fn
-    return parser_decorator
-
-def preprocess(fn):
-    "A decorator that marks this method as a preprocessor."
-    fn.is_a_preprocessor = True
-    return fn
+    def __init__(cls, name, bases, dictionary):
+        # make sure each subclass of the Parser has its own empty "_by_name"
+        # and "registered" attributes.
+        cls._by_name = {}
+        cls.registered = []
+        type.__init__(cls, name, bases, dictionary)
 
 
 class Parser(object):
-    "A base class for parsers."
     __metaclass__ = _meta_parser
-
-    def parse(self, token):
-        """Run all of the preprocessors on the input and check all of the
-        __parse_methods__ for a parser that matches this.
+        
+    @classmethod
+    def register(cls, pattern):
+        """This is a function that, given a regular expression pattern, returns
+        a decorator that registers its decorated function as a parse function
+        for this parser and wraps it in a function that raises Defer if the
+        pattern doesn't match.
         """
-        for pattern, method in self.__parse_methods__:
-            for pre in self.__preprocessors__:
-                token = pre(self, token)
-            m = re.match(pattern, token)
-            if m:
-                return method(self, *m.groups())
+        def parse_function_decorator(fn):
+            "This is a decorator that registers a parse function."
+
+            @wraps(fn)
+            def parse_function_wrapper(self, word, **kwargs):
+                # check the match.
+                match = re.match(pattern, word)
+                # if it matches, try running the function with the groups
+                if match:
+                    return fn(self, *match.groups(), **kwargs)
+                # if it doesn't, raise a Defer so that the parser knows to
+                # keep trying other things.
+                else:
+                    raise Defer()
+
+            # save this thing in a dict by its name and in an ordered list
+            cls._by_name[fn.__name__] = parse_function_wrapper
+            cls.registered.append(parse_function_wrapper)
+            return parse_function_wrapper
+
+        return parse_function_decorator
+
+    def parse(self, word):
+        "Try each registered parse function in turn to find one that matches."
+        for method in self.registered:
+            try:
+                return method(self, word)
+            except Defer:
+                # if the parse function raises a Defer, skip it but keep going
+                continue
         else:
-            raise ParserError(token)
+            # if nothing matches, raise a ParserError with the word.
+            raise ParserError(word)
 
+    def __getattr__(self, name):
+        """Address any given parse function like any other instance method, and
+        pass it self.
+        """
+        return partial(self._by_name[name], self)
 
+ 
 class ParserError(Exception):
     "Oh no, we don't know how to parse this thing."
     def __init__(self, value):
@@ -69,202 +73,8 @@ class ParserError(Exception):
         return repr(self.value)
 
 
-class ValueParser(Parser):
-    """Parse values. Values all return a value code and one other thing. The
-    one other thing can either be the next word they need or None.
+class Defer(Exception):
+    """Raise this error if this parsing function doesn't know how to parse some
+    thing, but it's possible for some other function to parse it.
     """
-    __metaclass__ = _meta_parser
-
-    registers = ["A", "B", "C", "X", "Y", "Z", "I", "J"]
-    rs = r"([a-cx-zijA-CX-ZIJ]{1})"
-
-    def __init__(self):
-        # a set to store all the labels we've seen
-        self.labels = set()
-
-    # values: all values return their value code and None or their next word
-    @parse(r"^%s$" % rs)
-    def register(self, name):
-        n = self.registers.index(name.upper())
-        return n, None
-
-    @parse(r"\[%s\]" % rs)
-    def register_pointer(self, name):
-        n = self.registers.index(name.upper())
-        return 0x08 + n, None
-
-    @parse(r"\[(.+)\s?\+\s?%s\]" % rs)
-    def register_plus_next_word(self, num, reg):
-        code = 0x10 + self.registers.index(reg.upper())
-        return code, literal_eval(num)
-
-    #TODO: case-insensitive for all of these
-    @parse(r"\[SP\+\+\]|POP|pop|\[sp\+\+\]")
-    def POP(self):
-        return 0x18, None
-
-    @parse(r"\[SP\]|PEEK|\[sp\]|peek")
-    def PEEK(self):
-        return 0x19, None
-
-    @parse(r"\[--SP\]|PUSH|\[--sp\]|push")
-    def PUSH(self):
-        return 0x1a, None
-
-    @parse("SP|sp")
-    def SP(self):
-        return 0x1b, None
-
-    @parse("PC|pc")
-    def PC(self):
-        return 0x1c, None
-
-    @parse("O|o")
-    def O(self):
-        return 0x1d, None
-
-    @parse(r"\[([^+]+)\]")
-    def next_word_pointer(self, num):
-        return 0x1e, literal_eval(num)
-
-    @parse(r"^([-+]?)(0x[0-9a-fA-F]+|0b[01]+|0o[0-7]+|[0-9]+)$")
-    def literal(self, sign, n):
-        num = literal_eval(n)
-        if sign == "-" and num != 0:
-            num = 0x10000 - num
-        if num > 0xffff:
-            raise ParserError(n)
-        elif num <= 0x1f:
-            return 0x20 + num, None
-        else:
-            return 0x1f, num
-
-    @parse(r"^([a-z]+)$")
-    def label(self, l):
-        self.labels.add(l)
-        # labels get passed through, to be dealt with later.
-        return 0x1f, l
-
-
-class AssemblyParser(Parser):
-    "Parse opcodes and instructions."
-    # why doesn't this get inherited from Parser?
-    __metaclass__ = _meta_parser
-    
-    cpu = DCPU16
-    opcodes = dict((v, k) for k, v in cpu.opcodes.iteritems())
-    special_opcodes = dict((v, k) for k, v in cpu.special_opcodes.iteritems())
-
-    @preprocess
-    def comments(self, inp):
-        "Remove comments and the whitespace up to them."
-        return re.sub(r"\s*;.*", "", inp)
-     
-    @preprocess
-    def whitespace(self, inp):
-        "Any whitespace leading, trailing, or more than one is insignificant."
-        inp = re.sub(r"^\s+", "", inp)
-        inp = re.sub(r"\s+$", "", inp)
-        return re.sub(r"\s{2,}", " ", inp)
-
-    def __init__(self):
-        self.values = ValueParser()
-
-    @parse(r"^\s*$")
-    def ignore(self):
-        return None,
-
-    def opcode(self, op):
-        "Look up an opcode."
-        gotten = self.opcodes.get(op.upper())
-        if gotten == None:
-            raise OpcodeError(op)
-        else:
-            return gotten
-
-    def special_opcode(self, op):
-        "Look up a special opcode."
-        gotten = self.special_opcodes.get(op.upper())
-        if gotten == None:
-            raise OpcodeError(op)
-        else:
-            return gotten
-
-    # ordinary instructions
-    @parse("^(\S+) ([^,]+)\,? (.+)$")
-    def instruction(self, op, a, b):
-        # get the value codes and extra words for each argument
-        a, first_word = self.values.parse(a)
-        b, second_word = self.values.parse(b)
-        # filter out Nones
-        not_nones = tuple(n for n in (first_word, second_word) if n != None)
-        # and then put them back at the end
-        nones = tuple(None for _ in range(2 - len(not_nones)))
-        return (self.opcode(op), a, b) + not_nones + nones
-
-    # special instructions
-    # (that horrible regex for the second argument is to allow spaces only
-    # inside of brackets with a +; this way, things stay unambiguous between
-    # ordinary and non-basic instructions, yet there can still be spaces inside
-    # brackets.)
-    @parse("^(\S+?),? (\S+|\[\S+\s\+\s\S+\])$")
-    def nonbasic_instructions(self, op, a):
-        a, first_word = self.values.parse(a)
-        return (0x0, self.special_opcode(op), a, first_word, None)
-
-    def parse_to_ints(self, line):
-        parsed = self.parse(line)
-        filtered = [p for p in parsed if p != None]
-        if len(filtered) >= 3:
-            word = from_opcode(*filtered[:3])
-            return [word] + filtered[3:]
-        else:
-            return []
-    
-    def labelled_or_not_instruction(self, instruction):
-        """Extract a label definition from an instruction, if it's there;
-        return that (or None) and the newly-unlabeled instruction.
-        """
-        m = re.match(r"^\s*(:(\w+))?\s*(.*)$", instruction)
-        if m:
-            return m.group(2), m.group(3) or None
-        else:
-            return None, instruction
-
-    def parse_iterable(self, iterable):
-        "Given an iterable of assembly code, parse each line."
-        labels = {}
-        code = []
-        for line in iterable:
-            label, instruction = self.labelled_or_not_instruction(line)
-            # if we got a label, remember that label and the address
-            if label != None:
-                labels[label] = len(code)
-            if instruction != None:
-                code.extend(self.parse_to_ints(instruction))
-        # get all the labels that the value parser has seen but that aren't
-        # defined in the code.
-        undefined_labels = [l for l in self.values.labels if l not in labels]
-        # if there are any, raise an error.
-        if len(undefined_labels) > 0:
-            raise LabelError(undefined_labels)
-        # and then pass through the code again, replacing labels with addresses
-        final = []
-        for word in code:
-            gotten = labels.get(word)
-            if gotten == None:
-                final.append(word)
-            else:
-                final.append(gotten)
-        #TODO: short labels
-        return final
-
-class LabelError(Exception):
-    def __init__(self, values):
-        self.values = values
-
-    def __str__(self):
-        if len(self.values) == 1:
-            return "%r is an undefined label." % self.values[0]
-        else:
-            return "%s are undefined labels." % ", ".join(self.values)
+    pass
