75ab0a8a9c43460e2f19b0d4773a4b2f5bb182f5
tim <tdixon51793@gmail.com>
2012-04-13 14:56:01 -0500

    separated the disassembly logic out of sixteen-dis.
    Now there's a subclass of the DCPU16 that disassembles.

---
+++ sixteen/dis.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from sixteen.dcpu16 import DCPU16
+
+
+class Disassembler(DCPU16):
+    "A subclass of the DCPU16 that disassembles."
+    def dis(self):
+        "Return an iterator over the code in memory, yielding it disassembled."
+        # until the rest of the RAM is 0x0000
+        while any(self.RAM[self.registers["PC"]:]):
+            address = self.registers["PC"]
+            next_word = self.get_next()
+            try:
+                # parse the opcodes and values out of the next word
+                opcode, values = self.parse_instruction(next_word)
+                # print each opcode and its arguments
+                assembly = "%s %s" % (opcode, ", ".join([v.dis for v in values]))
+            # if there's an OpcodeError, it's a DAT instruction.
+            except OpcodeError:
+                assembly = "DAT 0x%04x" % next_word
+            yield assembly, address
