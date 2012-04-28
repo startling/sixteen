6030c13f9decba1b1dd1d262b7322cb7bac20739
tim <tdixon51793@gmail.com>
2012-04-28 02:28:12 -0500

    added a dictionary of opcodes -> mnemonics

--- sixteen/dcpu16.py
+++ sixteen/dcpu16.py
@@ -22,10 +22,6 @@ class DCPU16(object):
         # initialize the RAM
         self.ram_init()
         
-    def cycle(self):
-        "Run for one instruction, returning the executed instruction."
-        pass
-
     def __getattr__(self, name):
         "If an attribute doesn't exist, try the registers."
         r = self.registers.get(name.upper())
@@ -37,3 +33,17 @@ class DCPU16(object):
     def ram_init(self):
         "A function that sets the RAM of this CPU to its initial values."
         self.ram = [0x0000] * self.cells
+
+    def cycle(self):
+        "Run for one instruction, returning the executed instruction."
+        pass
+
+    # a dictionary of opcode numbers to mnemonics
+    operations = {
+        0x00: "special", 0x01: "set", 0x02: "add", 0x03: "sub", 0x04: "mul",
+        0x05: "mli", 0x06: "div", 0x07: "dvi", 0x08: "mod", 0x09: "and",
+        0x0a: "bor", 0x0b: "xor", 0x0c: "shr", 0x0d: "asr", 0x0e: "shl",
+        0x0f: "sti", 0x10: "ifb", 0x11: "ifc", 0x12: "ife", 0x13: "ifn",
+        0x14: "ifg", 0x15: "ifa", 0x16: "ifl", 0x17: "ifu",
+        0x1a: "adx", 0x1b: "sbx",
+    }
