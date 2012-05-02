8513d12fb14a577f3f18f3a575c6871084e7782e
tim <tdixon51793@gmail.com>
2012-05-02 17:16:34 -0500

    expanded the keyboard device -- it should be up to spec now.

--- sixteen/devices.py
+++ sixteen/devices.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from collections import deque
+
 
 class Hardware(object):
     """Here's how hardware and interrupts work, if I've got it correctly.
@@ -53,18 +55,59 @@ class Keyboard(Hardware):
     version = 1
 
     def __init__(self):
-        self.mode = 0
-        self.interrupt = False
+        self.interrupt_mode = False
         self.message = None
+        # append when we get new keypresses, popleft to take them out
+        self.queue = deque()
     
     def on_interrupt(self, registers, ram):
-        self.mode = registers["A"]
-        if registers["A"] == 3:
-            if registers["B"] == 0:
-                self.interrupt = False
+        """From the docs for the generic keyboard:
+        0 - Clear keyboard buffer
+        1 - Store next key typed in C register, or 0 if the buffer is empty
+        2 - Set C register to 1 if the key specified by the B register is pressed, or
+            0 if it's not pressed
+        3 - If register B is non-zero, turn on interrupts with message B. If B
+            is zero, disable interrupts
+        """
+        if registers["A"] == 0:
+            self.queue.clear()
+        elif registers["A"] == 1:
+            #NOTE: this behavior might be wrong; the spec says to store the
+            # _next_ key, which is ambiguous.
+            if len(self.queue):
+                registers["C"] = self.queue.popleft()
             else:
-                self.interrupt = True
+                registers["C"] = 0
+        elif registers["A"] == 2:
+            value = self.queue.popleft()
+            registers["C"] = int(value == registers["B"])
+        elif registers["A"] == 3:
+            if registers["B"] == 0:
+                self.interrupt_mode = False
+            else:
+                self.interrupt_mode = True
                 self.message = registers["B"]
 
+    def register_keypress(self, keypress):
+        """Call this when you get a keypress from whatever source and want to add
+        it to the keyboard's buffer.
+        
+        This should be a number like the following (from the docs):
+        > Key numbers are:
+        > 0x10: Backspace
+        > 0x11: Return
+        > 0x12: Insert
+        > 0x13: Delete
+        > 0x20-0x7f: ASCII characters
+        > 0x80: Arrow up
+        > 0x81: Arrow down
+        > 0x82: Arrow left
+        > 0x83: Arrow right
+        > 0x90: Shift
+        > 0x91: Control
+        """
+        self.queue.append(keypress)
+        
     def on_cycle(self, changed_registers, changed_ram):
-        pass
+        if self.interrupt_mode and len(self.queue):
+            return self.message
