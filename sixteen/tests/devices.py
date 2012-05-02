211a1c9560072644a434b2510851735f3541ce2d
tim <tdixon51793@gmail.com>
2012-05-02 03:05:55 -0500

    added a test for interrupts

--- sixteen/tests/devices.py
+++ sixteen/tests/devices.py
@@ -17,6 +17,16 @@ class TestDevice(Hardware):
         registers["B"] = result
         registers["EX"] = overflow
 
+    def interrupt_next(self):
+        "A function to tell this device to interrupt on the next turn."
+        self._next = True
+
+    def on_cycle(self):
+        # if we've told it to raise an interrupt...
+        if self._next:
+            self._next = False
+            return 0xdead
+
 
 class DeviceTest(BaseDCPU16Test):
     def setUp(self):
@@ -56,3 +66,23 @@ class TestDeviceOperations(DeviceTest, unittest.TestCase):
         self.assertRegister("B", 4)
         self.assertRegister("EX", 0)
         self.assertRegister("A", 0xf0f0)
+
+    def test_interrupts(self):
+        self.cpu.ram[:5] = [
+            # ias, :handler
+            0x7d40, 0x0003,
+            # sub pc, 1
+            0x8b83, 
+            # handler: set pc, 0xff0f
+            0x7f81, 0xff0f
+        ]
+        # cycle 3 times
+        self.cpu.cycle()
+        self.cpu.cycle()
+        self.cpu.cycle()
+        # make the device interrupt
+        self.device.interrupt_next()
+        self.cpu.cycle()
+        self.cpu.cycle()
+        # make sure we got a message.
+        self.assertRegister("A", 0xdead)
