51113191f6691e66f013126b646134a1f55e626b
tim <tdixon51793@gmail.com>
2012-05-02 23:30:47 -0500

    call some callbacks that we're initialized with.

--- sixteen/screen.py
+++ sixteen/screen.py
@@ -15,7 +15,7 @@ class LEM1802(Hardware):
     version = 0x1802
     manufacturer = 0x1c6c8b36
 
-    def __init__(self):
+    def __init__(self, change_font, change_screen, change_palette):
         # initialize all the memory-mapping as nowhere
         self.mem_map_screen = None
         self.mem_map_font = None
@@ -24,6 +24,10 @@ class LEM1802(Hardware):
         self.font = characters[:]
         # and of the palette
         self.palette = palette[:]
+        # save the callbacks
+        self.change_font = change_font
+        self.change_screen = change_font
+        self.change_palette = change_palette
 
     def on_interrupt(self, registers, ram):
         "See docs/lem1802 for more details regarding these."
@@ -55,12 +59,25 @@ class LEM1802(Hardware):
             # if font memory-mapping is on and the address is in that region
             if self.mem_map_font is not None and (
                     0 <= addr - self.mem_map_font < len(self.font)):
-                pass
+                self.font[addr - self.mem_map_font] = value
+                self.change_font(addr, value)
             # if screen memory-mapping is on and the address is in that region
             if self.mem_map_screen is not None and (
                     0 <= addr - self.mem_map_screen < 0x182 ):
-                pass
+                # The LEM1802 has no internal video ram, but rather relies on being assigned
+                # an area of the DCPU-16 ram. The size of this area is 386 words, and is
+                # made up of 32x12 cells of the following bit format (in LSB-0):
+                #     ffffbbbbBccccccc
+                # The lowest 7 bits (ccccccc) select define character to display.
+                # ffff and bbbb select which foreground and background color to use.
+                # If B (bit 7) is set the character color will blink slowly.
+                foreground = (0b1111000000000000 & value) >> 12
+                background = (0b0000111100000000 & value) >> 8
+                blink = (0b0000000010000000 & value) >> 7
+                char = 0b1111111110000000 & value
+                self.change_screen(foreground, background, blink, char)
             # if palette memory-mapping is on and the address is in that region
             if self.mem_map_palette is not None and (
                     0 <= addr - self.mem_map_palette < len(self.palette)):
-                pass
+                self.palette[addr - self.mem_map_palette] = value
+                self.change_palette(addr, value)
