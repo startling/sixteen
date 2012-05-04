424c2048daaadf888e207b28d9d3ac1e7465832e
tim <tdixon51793@gmail.com>
2012-05-03 20:12:43 -0500

    get_instruction does all the work re: initializing values now.
    this lets us return more useful information, I think.

--- sixteen/dcpu16.py
+++ sixteen/dcpu16.py
@@ -8,17 +8,6 @@ from sixteen.bits import as_instruction, as_signed, from_signed
 from functools import wraps
 
 
-def basic_opcode(fn):
-    @wraps(fn)
-    def opcode_wrapper(self, state, b, a):
-        a_value = self.values[a](state, True)
-        b_value = self.values[b](state, False)
-        state.dis = "{0} {1}, {2}".format(fn.__name__, a_value.dis,
-                b_value.dis)
-        return fn(self, state, b_value, a_value)
-    return opcode_wrapper
-
-
 def signed(fn):
     """A decorator for basic operations that casts the arguments to signed
     (two's complement) integers and casts the single returned value back to
@@ -44,7 +33,6 @@ def set_value(fn):
     possibly EX. Each should return a tuple of their value and, optionally,
     the value for EX.
     """
-    @basic_opcode
     @wraps(fn)
     def set_wrapper(self, state, b, a):
         t = fn(self, state, b.get(), a.get())
@@ -61,18 +49,14 @@ def conditional(fn):
     @wraps(fn)
     def conditional_wrapper(self, state, b, a):
         # initialize the values
-        a_value = self.values[a](state, True)
-        b_value = self.values[b](state, False)
-        state.dis = "{0} {1}, {2}".format(fn.__name__, a_value.dis,
-                b_value.dis)
-        if fn(self, state, b_value.get(), a_value.get()):
+        if fn(self, state, b.get(), a.get()):
             # if the predicate returns True, we continue as usuaul
             return
         else:
             pc = state.registers["PC"]
             while True:
                 # run the next instruction so we can see how much it consumes
-                skip_state = self.get_instruction(pc)
+                _, _, skip_state = self.get_instruction(pc)
                 pc = skip_state.registers["PC"]
                 # if it's a conditional, continue
                 if not self.is_conditional(skip_state.consumed[0]):
@@ -88,15 +72,6 @@ def signed_conditional(fn):
     def signed_conditional_wrapper(self, state, b, a):
         return fn(self, state, as_signed(b), as_signed(a))
     return signed_conditional_wrapper
-
-
-def special_opcode(fn):
-    @wraps(fn)
-    def opcode_wrapper(self, state, a):
-        a_value = self.values[a](state, True)
-        state.dis = "{0} {1}".format(fn.__name__, a_value.dis)
-        return fn(self, state, a_value)
-    return opcode_wrapper
 
 
 class DCPU16(object):
@@ -165,17 +140,27 @@ class DCPU16(object):
         # unpack the opcode, a, and b
         op, b, a = as_instruction(next(state.ram_iter))
         # get the mnemonic and the method corresponding to it.
-        mnemonic = self.operations.get(op)
-        if mnemonic is None:
-            raise OpcodeError(opcode, location)
+        if op == 0:
+            mnemonic = self.special_operations.get(b)
+            if mnemonic is None:
+                raise OpcodeError(b, location)
+            a_value = self.values[a](state, True)
+            arguments = (a_value,)
+        else:
+            mnemonic = self.operations.get(op)
+            if mnemonic is None:
+                raise OpcodeError(opcode, location)
+            a_value = self.values[a](state, True)
+            b_value = self.values[b](state, False)
+            arguments = (b_value, a_value)
         method = getattr(self, mnemonic)
         # run the method and decide what changes to do.
-        method(state, b, a)
-        return state
+        return method, arguments, state
 
     def cycle(self):
         "Run for one instruction, returning the executed instruction."
-        state = self.get_instruction()
+        method, arguments, state = self.get_instruction()
+        method(state, *arguments)
         # hand hardware interrupts to devices
         for index in state.interrupts:
             device = self.hardware[index]
@@ -337,13 +322,11 @@ class DCPU16(object):
         overflow, result = divmod(b - a + state.registers["EX"], self.cells)
         return result, overflow and 0xffff
 
-    @basic_opcode
     def sti(self, state, b, a):
         b.set(a.get())
         state.registers["I"] += 1
         state.registers["J"] += 1
 
-    @basic_opcode
     def std(self, state, b, a):
         b.set(a.get())
         state.registers["I"] -= 1
@@ -363,39 +346,31 @@ class DCPU16(object):
         method = getattr(self, mnemonic)
         return method(state, a)
 
-    @special_opcode
     def jsr(self, state, a):
         state.push(state.registers["PC"])
         state.registers["PC"] = a.get()
 
-    @special_opcode
     def iag(self, state, a):
         a.set(state.registers["IA"])
 
-    @special_opcode
     def ias(self, state, a):
         state.registers["IA"] = a.get()
 
-    @special_opcode
     def int(self, state, a):
         "Triggers software interrupt with message A."
         state.interrupt(a.get())
 
-    @special_opcode
     def rfi(self, state, a):
         state.queuing = False
         state.registers["A"] = state.pop()
         state.registers["PC"] = state.pop()
 
-    @special_opcode
     def iaq(self, state, a):
         state.queuing = bool(a.get())
 
-    @special_opcode
     def hwn(self, state, a):
         a.set(len(self.hardware) % self.cells)
 
-    @special_opcode
     def hwq(self, state, a_value):
         """ From the docs:
         > sets A, B, C, X, Y registers to information about hardware a
@@ -415,7 +390,6 @@ class DCPU16(object):
             state.registers["Y"] = m_top
             state.registers["X"] = m_bottom
 
-    @special_opcode
     def hwi(self, state, a_value):
         a = a_value.get()
         if a < len(self.hardware):
