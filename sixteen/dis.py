1f7db21feb8dfc87ef7bd6e34bccd5acc015d482
tim <tdixon51793@gmail.com>
2012-05-03 20:41:44 -0500

    implemented sixteen.dis and sixteen-dis

--- sixteen/dis.py
+++ sixteen/dis.py
@@ -4,20 +4,11 @@ from sixteen.dcpu16 import DCPU16
 from sixteen.utilities import OpcodeError
 
 
-class Disassembler(DCPU16):
-    "A subclass of the DCPU16 that disassembles."
-    def dis(self):
-        "Return an iterator over the code in memory, yielding it disassembled."
-        # until the rest of the RAM is 0x0000
-        while any(self.RAM[self.registers["PC"]:]):
-            address = self.registers["PC"]
-            next_word = self.get_next()
-            try:
-                # parse the opcodes and values out of the next word
-                opcode, values = self.parse_instruction(next_word)
-                # print each opcode and its arguments
-                assembly = "%s %s" % (opcode, ", ".join([v.dis for v in values]))
-            # if there's an OpcodeError, it's a DAT instruction.
-            except OpcodeError:
-                assembly = "DAT 0x%04x" % next_word
-            yield assembly, address
+def disassembler(cpu=None):
+    "An iterator that disassembles."
+    cpu = cpu or DCPU16()
+    n = 0
+    while any(cpu.ram[n:]):
+        _, _, state = cpu.get_instruction(n)
+        yield state.dis, n
+        n = state.registers["PC"]
